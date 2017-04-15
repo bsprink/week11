63b1077bdad29bf8573ec19c8688962b5325f5a7
bsprink <bsprink@comcast.net>
2017-04-14 22:47:57 -0500

    modified bg, text, and functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 $("#button").click(function(){
-
-	$.getJSON("http//:api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=1f59466b416f7bac038a4e2f75e51019", function(data){
-		$('#temp').html(data.main[0].temp);
-	
+//alert();
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + $("#zipInputArea").val() + ",us&units=imperial&appid=1f59466b416f7bac038a4e2f75e51019", function(data){
+		$('#tempDisplay').html(data.main.temp);
+	});	
+			
 });
+
